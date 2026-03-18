d5a0d5b12492e5915997f1c61ba235a976c80d89
Brandon Parkerson <brandon.m.parkerson@gmail.com>
2026-03-18 21:53:11 +0200

    3 cards

--- frontend/src/Testimonials.jsx
+++ frontend/src/Testimonials.jsx
@@ -5,22 +5,22 @@ export default function Testimonials() {
     <>
       {/* Tesimonials Container*/}
 
-      <div className="my-5 border-solid bg-gray-800 border-4 border-black h-160 my-9 w-5/6 flex flex-col justify-around justify-self-center">
+      <div className="my-5 border-solid bg-gray-800 border-4 border-black h-160 my-9 w-5/6 flex flex-col justify-start gap-30 justify-self-center">
         <h1 className="text-center font-semibold text-white text-5xl text-balance">
           The Results Speak For Themselves
         </h1>
 
         {/* Cards Container */}
 
-        <div className="border-solid border-3 border-white flex justify-evenly ">
-          <div className="text-white">
-            <h2>Card 1</h2>
+        <div className="border-solid border-3 border-white flex justify-evenly gap-2 ">
+          <div className="text-white border-2 border-solid border-green w-1/3">
+            <h2 className="text-center">Card 1</h2>
           </div>
-          <div className="text-white">
-            <h2>Card 2</h2>
+          <div className="text-white border-2 border-solid border-green w-1/3">
+            <h2 className="text-center">Card 2</h2>
           </div>
-          <div className="text-white">
-            <h2>Card 3</h2>
+          <div className="text-white border-2 border-solid border-green w-1/3 ">
+            <h2 className="text-center">Card 3</h2>
           </div>
         </div>
       </div>
